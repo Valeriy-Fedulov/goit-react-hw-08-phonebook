b240c3da9a45d31e8919e703ee3fa1e6044f6b3b
vofm <80212894+Valeriy-Fedulov@users.noreply.github.com>
2021-12-08 17:04:12 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -2,42 +2,42 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import * as actions from './redux/actions';
 
 import { ContactForm } from './components/contactform';
 import { ContactList } from './components/contactlist';
 import { Filter } from './components/filter';
 
-function App() {
-  const [contacts, setContacts] = useState([]);
+function App(contacts, formSubmitHandler, deleteContact) {
+  // const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    localStorage.getItem('contacts') &&
-      setContacts(JSON.parse(localStorage.getItem('contacts')));
-  }, []);
+  // useEffect(() => {
+  //   localStorage.getItem('contacts') &&
+  //     setContacts(JSON.parse(localStorage.getItem('contacts')));
+  // }, []);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  function getVisibleContacts() {
-    const normalizedFilter = filter.toLowerCase();
+  // function getVisibleContacts() {
+  //   const normalizedFilter = filter.toLowerCase();
 
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
-    );
-  }
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(normalizedFilter),
+  //   );
+  // }
 
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactForm />
-      {/* <ContactForm contacts={contacts} onSubmit={formSubmitHandler} /> */}
+      <ContactForm contacts={contacts} onSubmit={formSubmitHandler} />
       <h2>Contacts</h2>
       <Filter filter={filter} setFilter={setFilter} />
       <ContactList
-        contacts={getVisibleContacts()}
-        // deleteContact={deleteContact}
+        // contacts={getVisibleContacts()}
+        deleteContact={deleteContact}
       />
     </>
   );
@@ -48,4 +48,17 @@ App.propTypes = {
   filter: PropTypes.string,
 };
 
-export default connect()(App);
+const mapStateToProps = state => {
+  return {
+    contacts: state.contacts,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    formSubmitHandler: () => dispatch(actions.formSubmitHandler()),
+    deleteContact: () => dispatch(actions.deleteContact()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
